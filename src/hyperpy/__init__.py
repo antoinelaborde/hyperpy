74d7cebda6cc82f8d66ce8c7e47eefda0f616741
Antoine <lab.antoine@gmail.com>
2021-05-04 21:51:26 +0200

    update notebooks import

--- src/hyperpy/__init__.py
+++ src/hyperpy/__init__.py
@@ -3,4 +3,17 @@ __version__ = "0.1.0"
 from hyperpy.loading import read_specim, read_hyspex, read_mat_file
 from hyperpy import preprocessing, spectral, utils
 from hyperpy.visu import BoxROIFigure, PCAFigure
-from hyperpy.preprocessing import spectral_process
+from hyperpy.preprocessing import (
+    spectral_process,
+    Log,
+    Positive,
+    StandardNormalVariate,
+    MeanCentering,
+    SavitzkyGolay,
+    MultiplicativeScatterCorrection,
+    Normalization,
+    DomainSelection
+)
+from hyperpy.models import kmeans_cube_plot, kmeans
+from hyperpy.spectral import SpectralMat, SpectralCube, RectangleMask
+from hyperpy.utils import DataSampler
